0857d5415d1f5f2e4371b866ff928738dcb1eb60
Prayoga BRD <103347873+prayogabrd@users.noreply.github.com>
2022-11-28 12:08:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 $(document).ready(() => {
-  $('body').css('margin', 0);
-  $('body').html('<iframe src="//insapgan.zidnirifan.my.id/"><iframe>');
-  $('iframe').css({
-    'border': 0,
-    'width': '100vw',
-    'height': '100vh'
-  });
+  window.location.href = 'https://beerde.rf.gd/';
 });
